18c05ef5c6fe8753bc5840a7d06f70619aefd051
Liu Jia <liujia95.me@gmail.com>
2017-06-09 15:50:08 +0800

    first commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package main.java;
+
+/**
+ * Created by Administrator on 2017/6/9 0009.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.print("main");
+    }
+
+
+}
